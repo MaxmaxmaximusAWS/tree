478704795be497b20942defdf2885814e4915a02
MaxmaxmaximusAWS <MaxmaxmaximusAWS@gmail.com>
2022-09-11 04:26:27 +0300

    refactor: code style

--- src/components/Tree/Tree.tsx
+++ src/components/Tree/Tree.tsx
@@ -10,7 +10,6 @@ export const Tree = ({ nodes: rawNodes }: TreeProps) => {
   // We create optimized node models for convenient work with data
   const nodes = useMemo(() => normalizeNodes(rawNodes), [rawNodes])
   const [activeNode, setActiveNode] = useState<Node>()
-
   const nodesWithStatuses = useMemo<Node[]>(() => {
     return [...updateNodesStatuses(nodes, activeNode)]
   }, [nodes, activeNode])
